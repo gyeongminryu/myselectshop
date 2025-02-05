9df0da52172acebc2ba7c104098c6a239b1a53d8
류경민 <rrarro000115@gmail.com>
2025-02-05 21:03:22 +0900

    상품 검색 API 구현

---
+++ src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
@@ -0,0 +1,25 @@
+package com.sparta.myselectshop.naver.controller;
+
+
+import com.sparta.myselectshop.naver.dto.ItemDto;
+import com.sparta.myselectshop.naver.service.NaverApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor //이거 사용하면 생성자 안 적어줘도 됨
+@RequestMapping("/api")
+public class NaverApiController {
+
+    private final NaverApiService naverApiService;
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam String query)  {
+        return naverApiService.searchItems(query);
+    }
+}
